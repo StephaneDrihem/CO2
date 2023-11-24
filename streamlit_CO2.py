9f988d5f7b1b2dbd743642feb9967420369fc724
Stéphane Drihem <132781862+StephaneDrihem@users.noreply.github.com>
2023-11-24 10:28:34 +0100

    Update streamlit_CO2.py

--- streamlit_CO2.py
+++ streamlit_CO2.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("Emissions de CO2")
+
+df2013 = pd.read_csv(CO2/gov2013.csv,encoding='latin1', sep=';')
+
+st.dataframe(df2013.head())
