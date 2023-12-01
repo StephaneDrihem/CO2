fda9fc22dbd1a0d5dbe3620e01e6214499028775
Stéphane Drihem <132781862+StephaneDrihem@users.noreply.github.com>
2023-12-01 15:32:37 +0100

    Update streamlit_CO2.py

--- streamlit_CO2.py
+++ streamlit_CO2.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import statsmodels.api
+
+
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression, ElasticNetCV
